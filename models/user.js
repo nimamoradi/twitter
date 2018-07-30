f1edb256c307431505155069d70b0c0ec23f14bd
nima <ni_moradi@yahoo.com>
2018-07-30 08:41:39 +0430

    user schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
     var User = sequelize.define('User', {
-        username: DataTypes.STRING
+        username: DataTypes.CHAR(15),
+        twitter_id:DataTypes.INTEGER(10),
+        bio:DataTypes.CHAR(160),
+        role:DataTypes.INTEGER(1),
+        bio_hastag:DataTypes.CHAR(160),
+
+
     });
+    //example usage
+    // uuid: {
+    //     type: DataTypes.UUID,
+    //     defaultValue: function() {
+    //       return generateMyId()
+    //     },
+    //     primaryKey: true
+    //   }
 
     User.associate = function(models) {
         models.User.hasMany(models.Task);
