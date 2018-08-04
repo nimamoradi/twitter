c3a55cc44ed7289fefbea1c3d100339b921931d7
nima <ni_moradi@yahoo.com>
2018-08-04 09:58:00 +0430

    get usre info

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var TweetsRouter = require('./routes/tweets');
+var user = require('./routes/getUser');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tweets', TweetsRouter);
+app.use('/getUser', user);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
